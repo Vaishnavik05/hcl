b91b040db24868af8722fa43c6a7c6db95b4589f
vaishu <vaishu@gmail.com>
2026-03-08 14:40:32 +0530

    Initial commit

---
+++ StudentDetails.java
@@ -0,0 +1,11 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name = "Vaishnavi";
+        int age = 20;
+        String course = "Computer Science";
+        System.out.println("Student Details:");
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
